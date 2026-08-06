3b7b0e2616037f53c38bc5810dca4c6d0a98b554
git-ksupriya <ksupriya2430@gmail.com>
2026-08-06 16:10:38 +0530

    feat: standardised category column

--- Personal_Finance/preprocessing/data_standardisation.py
+++ Personal_Finance/preprocessing/data_standardisation.py
@@ -1,3 +1,11 @@
+'''
+Standardize date formats
+Standardize category names
+Standardize payment modes
+Standardize locations
+Standardize transaction types
+'''
+
 import pandas as pd
 
 def inspect_unique_values(df):
@@ -14,9 +22,66 @@ def inspect_unique_values(df):
         print(f"Unique values: {df[col].nunique(dropna=False)}")
 
 
+def standardise_category(df):
+    CATEGORY_MAPPING = {
+    "FOOD": "Food",
+    "food": "Food",
+    "Foods": "Food",
+    "Foodd": "Food",
+    "Fod": "Food",
+
+    "rent": "Rent",
+    "RENT": "Rent",
+    "Rentt": "Rent",
+    "Rnt": "Rent",
+
+    "education": "Education",
+    "Educaton": "Education",
+    "EDU": "Education",
+
+    "Utilties":"Utilities",
+    "Utility":"Utilities",
+    "Utlities":"Utilities",
+    "utilities":"Utilities",
+
+    "entertainment":"Entertainment",
+    "Entertain":"Entertainment",
+    "Entrtnmnt":"Entertainment",
+
+    "HEALTH":"Health",
+    "Helth":"Health",
+    "health":"Health",
+
+    "Travl":"Travel",
+    "Traval":"Travel",
+    "TRAVEL":"Travel",
+    "travel":"Travel",
+
+    "Saving":"Savings",
+    "savings":"Savings",
+    "SAVINGS":"Savings",
+
+    "Other":"Others",
+    "OTHERS":"Others",
+    "others":"Others",
+    "Misc":"Others"
+
+    }
+
+    df["category"] = df["category"].replace(CATEGORY_MAPPING)
+    return df
+
+
 
 def main():
     df = pd.read_csv("processed/cleaned_dataset.csv")
+
+    # print(df["category"].isna().sum())  said 259 nan vals
+    df["category"] = df["category"].fillna("Others")
+    df = standardise_category(df)
+
+
+
     inspect_unique_values(df)
 
 
